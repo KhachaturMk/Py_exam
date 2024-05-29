73b394190b0c1eaa1b93bddbcf4d49cd777e9393
Khachatur <kmkrtchyan1986@gmail.com>
2024-05-29 14:44:25 +0400

    wip

---
+++ food_adn_drinks.py
@@ -0,0 +1,15 @@
+class FoodAndDrinks:
+    def __init__(self, name, price, ingredients):
+        self.name = name
+        self.price = price
+        self.ingredients = ingredients
+
+ChickenSandwich = FoodAndDrinks('Chicken Sandwich', 10, 'Chicken strips with Teriyaki glaze, served with fresh vegetables and sweet onion sauce.')
+VeganSandwich = FoodAndDrinks('Vegan Sandwich', 11, 'Spicy vegan patty with fresh vegetables with pickles and crispy onions topping.')
+SteakAndCheeseSandwich = FoodAndDrinks('Steak & Cheese Sandwich', 12, 'Portion of steak with american cheese and chipotle sauce.')
+TunaSandwich = FoodAndDrinks('Tuna Sandwich', 13, 'Tuna loaded sub with fresh vegetables and mayo sauce.')
+EggAndCheeseSandwich = FoodAndDrinks('Egg & Cheese Sandwich', 11, 'Poached egga and cheese, portion of veggies with mayo sauce.')
+Coca_Cola = FoodAndDrinks('Coca-Cola', 2, 'Carbonated soft drink')
+Fanta = FoodAndDrinks('Fanta', 2, 'Carbonated soft drink')
+Borjomi = FoodAndDrinks('Borjomi', 1, 'Mineral water')
+Likani = FoodAndDrinks('Likani', 1, 'Mineral water')
--- request.py
+++ request.py
@@ -1,14 +1,35 @@
-class Request:
-    db_request = []
-    def push(self, item):
-        self.db_request.append(item)
-    def pop(self):
-        self.db_request.pop()
-    def get(self):
-        if len(self.db_request) > 0:
-            return self.db_request[len(self.db_request) - 1]
-        return -1
-    def first(self):
-        if len(self.db_request) > 0:
-            return self.db_request[0]
-        return -1
+from food_adn_drinks import ChickenSandwich, VeganSandwich, SteakAndCheeseSandwich, TunaSandwich
+from food_adn_drinks import EggAndCheeseSandwich, Coca_Cola, Fanta, Borjomi, Likani
+
+requests = []
+
+def add():
+    while True:
+        item = input('Select item: ')
+        if item == '1':
+            requests.append(ChickenSandwich)
+        elif item == '2':
+            requests.append(VeganSandwich)
+        elif item == '3':
+            requests.append(SteakAndCheeseSandwich)
+        elif item == '4':
+            requests.append(TunaSandwich)
+        elif item == '5':
+            requests.append(EggAndCheeseSandwich)
+        elif item == '6':
+            requests.append(Coca_Cola)
+        elif item == '7':
+            requests.append(Fanta)
+        elif item == '8':
+            requests.append(Borjomi)
+        elif item == '9':
+            requests.append(Likani)
+        elif item == '0':
+            break
+        else:
+            print('incorrect input')
+
+add()
+
+for i in requests:
+    print(i.name)
