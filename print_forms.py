8ae98bb21a72736f272c85ce88507934fce732ca
Khachatur Mkrtchyan <kmkrtchyan1986@gmail.com>
2024-05-29 19:47:12 +0400

    wip

---
+++ print_forms.py
@@ -0,0 +1,26 @@
+def print_form1():
+    print('_______________________________________________')
+    print("Welcome to Sandwich City! Let's start ordering!")
+    print('Press 1 to add product')
+    print('Press 2 to remove product')
+    print('Press 3 to view order')
+    print('Press 4 to pay')
+    print('Press 0 to exit')
+
+def print_form2():
+    print('_______________________________________________')
+    print('Press 1 to add Chicken Sandwich')
+    print('Press 2 to add Vegan Sandwich')
+    print('Press 3 to add Steak & Cheese Sandwich')
+    print('Press 4 to add Tuna Sandwich')
+    print('Press 5 to add Egg & Cheese Sandwich')
+    print('Press 6 to add Coca-Cola')
+    print('Press 7 to add Fanta')
+    print('Press 8 to add Borjomi')
+    print('Press 9 to add Likani')
+    print('Press 0 to exit')
+
+
+
+
+print_form2()
