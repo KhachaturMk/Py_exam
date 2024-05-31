feca615b2d6dbf6a86cfa7f829bbe07a3b133e8c
Khachatur <kmkrtchyan1986@gmail.com>
2024-05-31 12:52:39 +0400

    wip

--- request.py
+++ request.py
@@ -8,31 +8,31 @@ def add(requests):
         item = input('Select product: -> ')
         if item == '1':
             requests.append(ChickenSandwich)
-            print('Chicken Sandwich is added in order')
+            print(ChickenSandwich.name, 'is added in order')
         elif item == '2':
             requests.append(VeganSandwich)
-            print('Vegan Sandwich is added in order')
+            print(VeganSandwich.name, 'is added in order')
         elif item == '3':
             requests.append(SteakAndCheeseSandwich)
-            print('Steak & Cheese Sandwich is added in order')
+            print(SteakAndCheeseSandwich.name, 'is added in order')
         elif item == '4':
             requests.append(TunaSandwich)
-            print('Tuna Sandwich is added in order')
+            print(TunaSandwich.name, 'is added in order')
         elif item == '5':
             requests.append(EggAndCheeseSandwich)
-            print('Egg & Cheese Sandwich is added in order')
+            print(EggAndCheeseSandwich.name, 'is added in order')
         elif item == '6':
             requests.append(Coca_Cola)
-            print('Coca-Cola is added in order')
+            print(Coca_Cola.name, 'is added in order')
         elif item == '7':
             requests.append(Fanta)
-            print('Fanta is added in order')
+            print(Fanta.name, 'is added in order')
         elif item == '8':
             requests.append(Borjomi)
-            print('Borjomi is added in order')
+            print(Borjomi.name, 'is added in order')
         elif item == '9':
             requests.append(Likani)
-            print('Likani is added in order')
+            print(Likani.name, 'is added in order')
         elif item == '0':
             break
         else:
