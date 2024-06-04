e73fb0075b55029b10da3a479133a4c6f95b496e
Khachatur <kmkrtchyan1986@gmail.com>
2024-06-04 17:49:40 +0400

    wip

--- check.py
+++ check.py
@@ -4,6 +4,9 @@ inp = input('Request number: ')
 try:
     df = pd.read_csv('report.csv')
     df_new = df[df['Request'] == inp]
-    print(df_new)
+    if df_new.empty:
+        print('Request not found')
+    else:
+        print(df_new)
 except:
-    print('file is empty')
+    print('File is empty')
